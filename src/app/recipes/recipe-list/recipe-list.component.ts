43e5a1b720bdf7a62daa88ebcc7481bdf2b6ecc6
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-02 17:13:58 +0100

    Added details option - now after clicking on  recipe, details of it will be displayed on the right

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,14 +6,19 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test', 'descriptionTest', 'https://i2.wp.com/wellplated.com/wp-content/uploads/2018/06/Baked-Salmon-in-Foil-at-400-600x847.jpg'),
-    new Recipe('Test', 'descriptionTest', 'https://i2.wp.com/wellplated.com/wp-content/uploads/2018/06/Baked-Salmon-in-Foil-at-400-600x847.jpg')
+    new Recipe('Test Two', 'descriptionTest Two', 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Kotlet_schabowy_56.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: []
+  templateUrl: './recipes.component.html'
 })
 export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
