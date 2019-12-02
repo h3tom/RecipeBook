ac2a1b83dc1c9a65c2bd363562c27aab9bbc83ff
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-02 15:45:57 +0100

    Moved recipe item from recipe-list html to recipe-item html thanks to property binding

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent implements OnInit {
+  @Input('recipeItem') recipe: Recipe;
 
   constructor() { }
 
