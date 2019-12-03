99b8377e3cb1ec3764cecf92b0ea6385fc6d4cc2
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-03 19:18:02 +0100

    Added option to add ingredients from recipe to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html'
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+    @Input() recipe: Recipe;
 
-  constructor() { }
+    constructor(private shoppingListService: ShoppingListService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onAddToShoppingList() {
+        this.shoppingListService.addIngredients(this.recipe.ingredients);
+    }
 
 }
