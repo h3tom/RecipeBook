4bef8d5c7b25721a773e5132a0126dbd9fcf10bf
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-03 16:56:35 +0100

    Changed Recipe model - it contains now Ingredients array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Test', 'descriptionTest', 'https://i2.wp.com/wellplated.com/wp-content/uploads/2018/06/Baked-Salmon-in-Foil-at-400-600x847.jpg'),
-        new Recipe('Test Two', 'descriptionTest Two', 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Kotlet_schabowy_56.jpg')
+        new Recipe(
+            'Test',
+            'descriptionTest',
+            'https://i2.wp.com/wellplated.com/wp-content/uploads/2018/06/Baked-Salmon-in-Foil-at-400-600x847.jpg',
+            [
+                new Ingredient('Salmon', 1),
+                new Ingredient('Lemon', 2)
+            ]),
+        new Recipe(
+            'Test Two',
+            'descriptionTest Two',
+            'https://upload.wikimedia.org/wikipedia/commons/a/ac/Kotlet_schabowy_56.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ])
     ];
 
     getRecipes() {
