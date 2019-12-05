491fdca578d4b85f8d2a3337d3a4313e0ea48708
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-05 17:32:30 +0100

    Added router-outlet and links, removed previous solutions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-    @Output() featureSelected = new EventEmitter<string>();
 
     constructor() { }
 
     ngOnInit() {
-    }
-
-    onSelect(feature: string) {
-        this.featureSelected.emit(feature);
     }
 
 }
