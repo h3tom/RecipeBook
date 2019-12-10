449bbc9154bae45c03b6351228e5788d55554cf7
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-10 22:44:23 +0100

    Added delete option in shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -21,10 +21,10 @@ export class RecipeDetailComponent implements OnInit {
     ngOnInit() {
         this.route.params.subscribe(
             (params: Params) => {
-                this.id = +params['id'];
+                this.id = +params.id;
                 this.recipe = this.recipeService.getRecipe(this.id);
             }
-        )
+        );
     }
 
     onAddToShoppingList() {
