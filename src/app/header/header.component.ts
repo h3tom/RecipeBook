b3e0fde6b2f1a6e2d172c6fd1844a5c16b2cd80d
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-17 22:43:01 +0100

    Added logic to fetching data in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor() { }
+    constructor(private dataStorageService: DataStorageService) { }
 
     ngOnInit() {
+    }
+
+    onFetch() {
+        this.dataStorageService.fetchRecipesFromDB();
     }
 
 }
