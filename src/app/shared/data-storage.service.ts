255c302b1601de950f93d8d6171ac5d3e5fbd79e
Tomasz Hein <hein.tomasz@gmail.com>
2019-12-17 22:42:03 +0100

    Added new service which connects to back end

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable()
+export class DataStorageService {
+
+    constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+    fetchRecipesFromDB() {
+        this.http.get<Recipe[]>('https://hetom-recipebookbackend.herokuapp.com/recipe/all').subscribe(
+            (recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+        )
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.http.post('https://hetom-recipebookbackend.herokuapp.com/recipe', recipe).subscribe(
+            (response => {
+                if (response === 'OK') {
+                    alert('Recipe added');
+                    this.fetchRecipesFromDB();
+                } else {
+                    alert('Could not add Recipe')
+                }
+            })
+        )
+    }
+
+    updateRecipe(recipe: Recipe) {
+        this.http.put('https://hetom-recipebookbackend.herokuapp.com/recipe', recipe).subscribe(
+            (response => {
+                if (response === 'OK') {
+                    alert('Recipe updated')
+                    this.fetchRecipesFromDB();
+                } else {
+                    alert('Could not update Recipe')
+                }
+            })
+        )
+    }
+
+    deleteRecipe(id: number) {
+        this.http.delete('https://hetom-recipebookbackend.herokuapp.com/recipe/' + id).subscribe(
+            (response => {
+                if (response === 'OK') {
+                    alert('Recipe deleted');
+                    this.fetchRecipesFromDB();
+                } else {
+                    alert('Could not delete Recipe')
+                }
+            })
+        );
+    }
+
+}
