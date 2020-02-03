a2653ce9ee501e12007af3045e2f72472f4dbd34
Tomasz Hein <hein.tomasz@gmail.com>
2020-02-03 14:25:04 +0100

    Updated header -> application changes header based on authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+    private userSubscription: Subscription;
+    isAuthenticated = false;
 
-    constructor(private dataStorageService: DataStorageService) { }
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
 
     ngOnInit() {
+        this.authService.user.subscribe(
+            user => {
+                this.isAuthenticated = !user ? false : true;
+            }
+        );
     }
 
     onFetch() {
         this.dataStorageService.fetchRecipesFromDB().subscribe();
+    }
+
+    onLogout() {
+        this.authService.logout();
+    }
+
+    ngOnDestroy() {
+        this.userSubscription.unsubscribe();
     }
 
 }
