1ba49ab8ea7107af025cd0f95d7b9e7dbc64790e
Tomasz Hein <hein.tomasz@gmail.com>
2020-02-05 11:55:59 +0100

    Heroku test deployment 5

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/recipe-book'));
+app.use(express.static(__dirname + '/dist/RecipeBook'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/recipe-book/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/RecipeBook/index.html'));
 });
 
 // default Heroku PORT
